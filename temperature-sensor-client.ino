98e7d7ab7aa313f22acb4dafd8cb6eb7e75b93b9
etienne <etienne@local>
2020-11-10 13:05:47 +0100

    Initial main file - setup and loop methods - basic wifi client Wifi Config file - contains SSID and Password

---
+++ temperature-sensor-client.ino
@@ -0,0 +1,33 @@
+#include "wifi-config.h"
+#include "ESP8266WiFi.h"
+
+const char* ssid     = STASSID;
+const char* password = STAPSK;
+
+void setup() {
+  Serial.begin(115200);
+
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.println("Setup done");
+
+}
+
+void loop() {
+  
+  
+}
