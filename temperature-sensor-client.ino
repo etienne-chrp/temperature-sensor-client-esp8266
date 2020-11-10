3ef53dbc12e718da1fd7716fce66bed466d95a1a
etienne <etienne@local>
2020-11-10 14:55:11 +0100

    Add basic temperature and humidity reading from DHT sensor

--- temperature-sensor-client.ino
+++ temperature-sensor-client.ino
@@ -1,6 +1,11 @@
 #include "wifi-config.h"
 
+#include <DHTesp.h>
 #include <ESP8266WiFi.h>
+
+#define DHT11_PIN 2
+
+DHTesp dht;
 
 const char* ssid     = STASSID;
 const char* password = STAPSK;
@@ -25,11 +30,20 @@ void setup() {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
+  dht.setup(4, DHTesp::DHT11); // Connect DHT sensor to GPIO 4
+
   Serial.println("Setup done");
 
 }
 
 void loop() {
-  
-  
+  delay(dht.getMinimumSamplingPeriod());
+
+  Serial.println(dht.getStatusString());
+  Serial.print("Temperature: ");
+  Serial.println(dht.getTemperature(), 1);
+  Serial.print("Humidity: ");
+  Serial.println(dht.getHumidity()), 1;
+
+  delay(1000);
 }
